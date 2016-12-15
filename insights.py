1c1c725b5d8bdaf98990dc608795085453f96716
Will Schumacher <meintodengel@gmail.com>
2016-12-15 11:31:53 -0800

    add split by date and some aggregate

--- insights.py
+++ insights.py
@@ -1,23 +1,90 @@
-
+import re
+import dateutil.parser
+from datetime import date, timedelta
 
 class Insights:
     def __init__(self, data):
         self.data = data
+        self.dateRegex = r".+?(?=T)"
 
     def process(self):
         print('Harvesting...')
         results = [];
 
-        if len(data) < 1:
+        if len(self.data) < 1:
+            pass
             # throw an error
-        elif len(data) == 1:
-            # split by date
+        elif len(self.data) == 1:
+            pass
+            days = self.splitByDate(self.data[0])
             # check contiguous
-        elif len(data) == 2:
+        elif len(self.data) == 2:
+            pass
             # compare
             # return
         else:
+            pass
             # check contiguous
 
+        self.aggregate(days[0:])
 
+        # Harvest some shit
         return self.data
+
+    def splitByDate(self, data):
+        result = []
+        dates = {x[0] for x in data['rows']}
+
+        for date in dates:
+            if len(date) > 10:
+                dDate = re.match(self.dateRegex, date).group()
+            else:
+                dDate = date
+            res = {}
+            res['query'] = {}
+            res['query']['start-date'] = dDate
+            res['query']['end-date'] = dDate
+            res['query']['dimensions'] = data['query']['dimensions']
+            res['query']['metrics'] = data['query']['metrics']
+            res['columnHeaders'] = data['columnHeaders'][1:]
+            res['rows'] = [x[1:] for x in data['rows'] if x[0] == date]
+            result.append(res)
+
+
+        result = sorted(result, key=lambda d: map(int, d['query']['start-date'].split('-')))
+
+        return result
+
+    def checkContiguousDates(self, data):
+        lastDate = ''
+        oneDay = timedelta(day=1)
+
+    def aggregate(self, data):
+        agg = {}
+        meta = {}
+        agg['meta'] = {}
+        dimensions = data[0]['query']['dimensions']
+        metrics = data[0]['query']['metrics']
+        for met in metrics:
+            agg[met] = {}
+
+        for day in data:
+            startDate = dateutil.parser.parse(day['query']['start-date'])
+            endDate = dateutil.parser.parse(day['query']['end-date'])
+
+            if startDate != endDate:
+                raise Exception('Harvestor received non exploded data')
+
+            if 'startDate' not in meta:
+                meta['startDate'] = startDate
+
+            if 'endDate' not in meta:
+                meta['endDate'] = endDate
+
+            if startDate < meta['startDate']:
+                meta['startDate'] = startDate
+
+            if endDate > meta['endDate']:
+                meta['endDate'] = endDate
+
+        print meta
--- test.js
+++ test.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const iClient = require('./insightClient');
 ic = new iClient('queue');
 
-// let res = fs.readFileSync('./newTestData.json', 'utf8');
-// res = JSON.parse(res);
+let res = fs.readFileSync('./testData.json', 'utf8');
+res = JSON.parse(res);
 
-ic.send('queue', 'res', (o) => {
+ic.send('queue', res, (o) => {
   if (o.error) console.log(o.error);
   console.log(o.payload);
   console.log('done');
