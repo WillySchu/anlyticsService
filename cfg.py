a7fe20e32e390df84758503d5787e862670d7c45
Will Schumacher <meintodengel@gmail.com>
2017-01-09 14:16:25 -0800

    update cfg to work and use getter

--- cfg.py
+++ cfg.py
@@ -1,7 +1,11 @@
 import os
 import json
 
+# Config class gets access to proper config information
+# it makes it 'gettable' by defining the standard __getitem__ method
 class Config(object):
+    # finds path to config repo on init and gets appropriate config file
+    # for the environment
     def __init__(self):
         filePath = os.path.realpath(__file__)
         dirPath = os.path.dirname(filePath)
@@ -9,15 +13,13 @@ class Config(object):
         self.configDir = os.path.join(baseDir, 'config')
         env = os.environ['NODE_ENV']
         env = env if env in ['development', 'staging', 'production'] else 'development'
-        configFilePath = '/node/config.json'
-        self.configFile = os.path.join(self.configDir, envDir, configFilePath)
+        configFilePath = 'node/config.json'
+        self.configFile = os.path.join(self.configDir, env, configFilePath)
 
         with open(self.configFile) as configJSON:
-            self.cfg = json.load(configJSON)
+            self.cfg = json.load(configJSON)['env'][env]
 
-    def get(self, key=None):
-        if key is None:
-            return self.cfg
-        if key in config:
+    def __getitem__(self, key):
+        if key in self.cfg:
             return self.cfg[key]
         return {}
