4b0a8a9fac5ab2f9962ad4c1f0fa00b453a9a332
Will Schumacher <meintodengel@gmail.com>
2017-01-11 14:34:37 -0800

    config

--- cfg.py
+++ cfg.py
@@ -7,19 +7,17 @@ class Config(object):
     # finds path to config repo on init and gets appropriate config file
     # for the environment
     def __init__(self):
+        filePath = os.path.realpath(__file__)
+        dirPath = os.path.dirname(filePath)
+        baseDir = os.path.split(dirPath)[0]
+        self.configDir = os.path.join(baseDir, 'config')
         try:
             env = os.environ['NODE_ENV']
         except:
             env = 'development'
         self.env = env if env in ['development', 'staging', 'production'] else 'development'
-
-        if self.env == 'development':
-            configDir = '/home/ubuntu/Dropbox/code/local/config'
-        else:
-            configDir = '/home/ubuntu/config'
-
         configFilePath = 'node/config.json'
-        self.configFile = os.path.join(configDir, self.env, configFilePath)
+        self.configFile = os.path.join(self.configDir, self.env, configFilePath)
 
         with open(self.configFile) as configJSON:
             self.cfg = json.load(configJSON)['env'][self.env]
