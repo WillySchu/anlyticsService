352105f11bf5951a064c7c69cb169a380d141209
Will Schumacher <meintodengel@gmail.com>
2017-01-11 11:51:51 -0800

    add logging wrapper

---
+++ log.py
@@ -0,0 +1,21 @@
+import logging
+import os
+
+from cfg import Config
+
+cfg = Config()
+
+log_dir = '/home/ubuntu/logs'
+
+class Log(logging.Logger):
+    def __init__(self):
+        logging.Logger.__init__(self, __name__)
+        self.setLevel(logging.DEBUG)
+
+        sh = logging.StreamHandler()
+        self.addHandler(sh)
+        if cfg.env != 'development':
+            log_file = os.path.join(log_dir, 'analyticsService.log')
+            fh = logging.FileHandler(log_file)
+            fh.setLevel(logging.INFO)
+            self.addHandler(fh)
