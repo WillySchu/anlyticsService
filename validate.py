e187a186c26d4da4f07a32a9c7a9070ed81b6dcd
Will Schumacher <meintodengel@gmail.com>
2017-03-01 09:41:40 -0800

    add benchmark class

--- validate.py
+++ validate.py
@@ -7,35 +7,39 @@ import numpy as np
 
 from scipy.optimize import brute
 
-def test(file):
-    data = pd.read_csv(file, sep='\t', parse_dates=[1])
+class Benchmark(object):
+    def __init__(self, file, sep='\t', date_col=1):
+        self.data = pd.read_csv(file, sep=sep, parse_dates=[date_col])
+        cols = list(self.data.columns.values)
+        self.dim = cols[0]
+        self.met = cols[-1]
+        self.data = self.data.groupby([self.dim, 'ga:date']).sum()
 
-    dims = list(set(data['ga:source']))
+    def test(self):
+        dims = list(set(self.data[self.dim]))
+        sess = self.data[self.met]
 
-    data = data.groupby(['ga:source', 'ga:date']).sum()
-    sess = data['ga:sessions']
+        sum_rmse = 0
+        t0 = arrow.get()
 
-    sum_rmse = 0
-    t0 = arrow.get()
+        for dim in dims:
+            l = len(sess[dim])
+            if l < 82:
+                continue
+            vl = l // 5
 
-    for dim in dims:
-        l = len(sess[dim])
-        if l < 82:
-            continue
-        vl = l // 5
+            start = sess[dim][-vl:].index[0].strftime('%Y-%m-%d')
 
-        start = sess[dim][-vl:].index[0].strftime('%Y-%m-%d')
+            model = auto_arima(sess[dim][:l-vl+1])
+            pred = model.get_prediction(start=start, end=l-1)
+            resid = pred.predicted_mean - sess[dim][-vl:]
 
-        model = auto_arima(sess[dim][:l-vl+1])
-        pred = model.get_prediction(start=start, end=l-1)
-        resid = pred.predicted_mean - sess[dim][-vl:]
+            sum_rmse += rmse(resid)
 
-        sum_rmse += rmse(resid)
+        tf = arrow.get()
+        duration = tf - t0
 
-    tf = arrow.get()
-    durration = tf - t0
-
-    return (duration, sum_rmse)
+        return (duration, sum_rmse)
 
 def test_arima(orders, x):
     order = orders[:3]
@@ -69,3 +73,11 @@ def auto_arima(x):
             # multi day long periods
             seasonal_order = np.insert(seasonal_order, 3, 7)
             return sm.tsa.statespace.SARIMAX(x.astype(float), trend='n', order=order, seasonal_order=seasonal_order).fit()
+
+def clean_file(file, char='\x00'):
+    h = open(file, 'r')
+    f = h.read()
+    f = ''.join(f.split(char))
+    w = open(file, 'w')
+    w.write(f)
+    w.close()
